0c3d18bca5e14f0a9fad0642b40fec68ae14d486
Eren ÖZTEN <priimalshade@gmail.com>
2020-11-26 17:45:21 +0300

    startup.js ve json değiştirildi

--- TheApplication/Startup.cs
+++ TheApplication/Startup.cs
@@ -27,9 +27,13 @@ namespace TheApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(Configuration.GetConnectionString("ConnectionName")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
